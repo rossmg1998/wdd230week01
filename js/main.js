348c6484784f463dc8e71cc07e122f5ba7a1063e
Use your GitHub username <Enter your email address>
2022-10-10 17:46:45 -0600

    Removed files

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-document.querySelector("footer div span").textContent = today.getFullYear();
-document.getElementById("lastupdatedate").textContent = document.lastModified;
-
-// const lastModified = new Date(document.lastModified);
-// document.getElementById("lastupdatedate").textContent = `${lastModified.getDate()}/${lastModified.getMonth()}/${lastModified.getFullYear()}`;
